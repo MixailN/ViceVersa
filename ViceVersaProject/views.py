aad423ca33fb00f3e90caac194b600999569b0a2
Michael <mixail_nabiullin@mail.ru>
2021-02-18 19:21:48 +0300

    Added words count

--- ViceVersaProject/views.py
+++ ViceVersaProject/views.py
@@ -8,4 +8,6 @@ def home(request):
 def reverse(request):
     user_text = request.GET['usertext']
     reversed_text = user_text[::-1]
-    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text})
+    words_quantity = len(user_text.split())
+    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text,
+                                            'wordsquantity': words_quantity})
